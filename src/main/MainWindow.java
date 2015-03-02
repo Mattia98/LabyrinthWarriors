fd5faeb6bc74b33f83b766db03245167895afbff
Mattia98 <io.mattia98@gmail.com>
2015-03-02 15:20:40 +0100

    --V0.4--
    + WALLS ARE LOADED FROM A FILE!!!11one
    ! Fixed Warrior can not go left when wall under it
    
    A test mapfile was added too (map.json)
    P.S.: Sorry, forgot the json lib ;)

--- src/main/MainWindow.java
+++ src/main/MainWindow.java
@@ -1,13 +1,13 @@
 package main;
 
 import java.awt.Color;
-import java.awt.Component;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
-import javax.swing.*;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class MainWindow extends JFrame implements ActionListener, KeyListener {
 	
